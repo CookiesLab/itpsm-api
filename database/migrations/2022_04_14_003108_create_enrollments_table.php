6b90e1dcb7c16821eebef184d3d4dcd43dc5e445
EdwinLovo <edwinlovo.dev@gmail.com>
2022-04-13 19:12:20 -0600

    [ADDED] Enrollment-related migrations

--- database/migrations/2022_04_14_003108_create_enrollments_table.php
+++ database/migrations/2022_04_14_003108_create_enrollments_table.php
@@ -13,8 +13,83 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('enrollments', function (Blueprint $table) {
+        Schema::create('periods', function (Blueprint $table) {
             $table->id();
+            $table->integer('code');
+            $table->integer('year');
+            
+            $table->timestamps();
+        });
+
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id('code');
+            $table->integer('quota');
+            $table->string('schedule', 255);
+
+            $table->unsignedBigInteger('teacher_id')->index();
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+
+            /**
+             * Fields with missing Primary Key
+             */
+            $table->unsignedBigInteger('curriculum_subject_id')->index();
+            $table->foreign('curriculum_subject_id')->references('id')->on('curriculum_subjects');
+
+            $table->unsignedBigInteger('period_id')->index();
+            $table->foreign('period_id')->references('id')->on('periods');
+            
+            $table->timestamps();
+        });
+
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->text('description')->nullable();
+            $table->date('date');
+            $table->float('porcentaje');
+
+            $table->unsignedBigInteger('section_id')->index();
+            $table->foreign('section_id')->references('code')->on('sections');
+            
+            $table->timestamps();
+        });
+
+        Schema::create('score_evaluations', function (Blueprint $table) {
+            $table->float('score');
+
+            /**
+             * Fields with missing Primary Key
+             */
+            $table->unsignedBigInteger('student_id')->index();
+            $table->foreign('student_id')->references('id')->on('students');
+
+            $table->unsignedBigInteger('evaluation_id')->index();
+            $table->foreign('evaluation_id')->references('id')->on('evaluations');
+            
+            $table->timestamps();
+        });
+
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->float('final_score');
+            $table->boolean('is_approved');
+            $table->integer('enrollment');
+            $table->unsignedBigInteger('teacher_id');
+
+            /**
+             * Fields with missing Primary Key
+             */
+            $table->unsignedBigInteger('curriculum_subject_id')->index();
+            $table->foreign('curriculum_subject_id')->references('id')->on('curriculum_subjects');
+
+            $table->unsignedBigInteger('period_id')->index();
+            $table->foreign('period_id')->references('id')->on('periods');
+            
+            $table->unsignedBigInteger('code')->index();
+            $table->foreign('code')->references('code')->on('sections');
+
+            $table->unsignedBigInteger('student_id')->index();
+            $table->foreign('student_id')->references('id')->on('students');
+
             $table->timestamps();
         });
     }
@@ -27,5 +102,9 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('enrollments');
+        Schema::dropIfExists('periods');
+        Schema::dropIfExists('sections');
+        Schema::dropIfExists('evaluations');
+        Schema::dropIfExists('score_evaluations');
     }
 };
