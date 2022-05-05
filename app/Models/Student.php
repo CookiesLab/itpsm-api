f19c891d3b8eabeaf06b42b5f005bc7d718dcf9f
EdwinLovo <edwinlovo.dev@gmail.com>
2022-05-04 19:00:36 -0600

    [ADDED] student fillable fields

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -9,4 +9,38 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'carnet',
+        'name',
+        'last_name',
+        'email',
+        'birth_date',
+        'address',
+        'phone_number',
+        'home_phone_number',
+        'gender',
+        'relationship',
+        'status',
+        'blood_type',
+        'mother_name',
+        'mother_phone_number',
+        'father_name',
+        'father_phone_number',
+        'emergency_contact_name',
+        'emergency_contact_phone',
+        'diseases',
+        'allergies',
+        'entry_date',
+        'date_high_school_degree',
+        'municipality_id',
+        'department_id',
+        'country_id',
+        'status_id'
+    ];
 }
