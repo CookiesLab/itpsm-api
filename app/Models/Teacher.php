83fd2849c8e121444754675aca12724c23140531
EdwinLovo <edwinlovo.dev@gmail.com>
2022-05-04 18:02:22 -0600

    [ADDED] fillable fields for teacher

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -9,4 +9,28 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'last_name',
+        'birth_date',
+        'nit',
+        'dui',
+        'isss_number',
+        'nup_number',
+        'email',
+        'genre',
+        'address',
+        'phone_number',
+        'home_phone_number',
+        'municipality_id',
+        'department_id',
+        'country_id',
+        'status_id'
+    ];
 }
