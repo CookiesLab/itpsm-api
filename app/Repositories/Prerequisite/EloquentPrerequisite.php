53f37d498077a122ef11436cea60c541802a639d
EdwinLovo <edwinlovo.dev@gmail.com>
2022-05-05 21:06:13 -0600

    [ADDED] Prerequisite repo and service

---
+++ app/Repositories/Prerequisite/EloquentPrerequisite.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * @file
+ * EloquentPrerequisite
+ *
+ * All code is copyright by the original authors and released under the GNU Aferro General Public License version 3 (AGPLv3) or later.
+ * See COPYRIGHT and LICENSE.
+ */
+
+namespace App\Repositories\Prerequisite;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+use App\Models\Prerequisite;
+
+class EloquentPrerequisite implements PrerequisiteInterface
+{
+
+  /**
+   * Prerequisite
+   *
+   * @var App\Models\Prerequisite;
+   *
+   */
+  protected $Prerequisite;
+
+  /**
+   * DB
+   *
+   * @var Illuminate\Support\Facades\DB;
+   *
+   */
+  protected $DB;
+
+  public function __construct(Model $Prerequisite, DB $DB)
+  {
+    $this->Prerequisite = $Prerequisite;
+    $this->DB = $DB;
+  }
+
+  /**
+   * Retrieve list of Prerequisites
+   *
+   * @return Illuminate\Database\Eloquent\Collection
+   */
+  public function searchTableRowsWithPagination($count = false, $limit = null, $offset = null, $filter = null, $sortColumn = null, $sortOrder = null)
+  {
+    $query = $this->DB::table('prerequisites AS p')
+      ->select(
+        'p.curriculum_subject_id',
+        'p.prerequisite_id',
+        's.id AS subject_id',
+        's.name AS subject_name',
+        's.code AS subject_code',
+        'c.id AS curricula_id',
+        'c.name AS curricula_name',
+        'c.year AS curricula_year',
+        'c.is_active, AS curricula_is_active'
+      )
+      ->join('curriculum_subjects as cs', 'p.curriculum_subject_id', '=', 'cs.id')
+      ->join('subjects as s', 'cs.subject_id', '=', 's.id')
+      ->join('curricula as c', 'cs.curriculum_id', '=', 'c.id');
+
+
+    if (!empty($filter)) {
+      $query->where(function ($dbQuery) use ($filter) {
+        foreach (['name', 'email'] as $key => $value) {
+          $dbQuery->orWhere($value, 'like', '%' . str_replace(' ', '%', $filter) . '%');
+          //$dbQuery->orwhereRaw('lower(`' . $value . '`) LIKE ? ',['%' . strtolower(str_replace(' ', '%', $filter)) . '%']);
+        }
+      });
+    }
+
+    if (!empty($sortColumn) && !empty($sortOrder)) {
+      $query->orderBy($sortColumn, $sortOrder);
+    }
+
+    if ($count) {
+      return $query->count();
+    }
+
+    if (!empty($limit)) {
+      $query->take($limit);
+    }
+
+    if (!empty($offset) && $offset != 0) {
+      $query->skip($offset);
+    }
+    return new Collection(
+      $query->get()
+    );
+  }
+
+  /**
+   * Get Prerequisites by subjectId
+   *
+   * @param  int $id
+   *
+   * @return App\Models\Collection
+   */
+  public function byCurriculumSubjectId($id)
+  {
+    return $this->Prerequisite
+    ->where('curriculum_subject_id', '=', $id)
+    ->get();
+  }
+
+  /**
+   * Create a new Prerequisite
+   *
+   * @param array $data
+   * 	An array as follows: array('field0'=>$field0, 'field1'=>$field1);
+   *
+   * @return App\Models\Prerequisite $Prerequisite
+   */
+  public function create(array $data)
+  {
+    $prerequisite = new Prerequisite();
+    $prerequisite->fill($data)->save();
+
+    return $prerequisite;
+  }
+
+  /**
+   * Update an existing Prerequisite
+   *
+   * @param array $data
+   * 	An array as follows: array('field0'=>$field0, 'field1'=>$field1);
+   *
+   * @param App\Models\Prerequisite $Prerequisite
+   *
+   * @return boolean
+   */
+  public function update(array $data, $prerequisite = null)
+  {
+    if (empty($prerequisite)) {
+      $prerequisite = $this->byId($data['id']);
+    }
+
+    return $teacher->update($data);
+  }
+
+  /**
+   * Delete existing Prerequisite
+   *
+   * @param integer $id
+   * 	An Prerequisite id
+   *
+   * @return boolean
+   */
+  public function delete($id)
+  {
+    return $this->Prerequisite->destroy($id);
+  }
+}
---
+++ app/Services/Prerequisite/PrerequisiteManager.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * @file
+ * Prerequisite Management Interface Implementation.
+ *
+ * All ModuleName code is copyright by the original authors and released under the GNU Aferro General Public License version 3 (AGPLv3) or later.
+ * See COPYRIGHT and LICENSE.
+ */
+
+namespace App\Services\Prerequisite;
+
+use App\Repositories\Prerequisite\PrerequisiteInterface;
+use Carbon\Carbon;
+
+class PrerequisiteManager
+{
+  /**
+   * Prerequisite
+   *
+   * @var App\Repositories\Prerequisite\PrerequisiteInterface;
+   *
+   */
+  protected $Prerequisite;
+
+  /**
+   * Carbon instance
+   *
+   * @var Carbon\Carbon
+   *
+   */
+  protected $Carbon;
+
+  /**
+   * responseType
+   *
+   * @var String
+   *
+   */
+  protected $responseType;
+
+  public function __construct(
+    PrerequisiteInterface $Prerequisite,
+    Carbon $Carbon
+  ) {
+    $this->Prerequisite = $Prerequisite;
+    $this->Carbon = $Carbon;
+    $this->responseType = 'prerequisites';
+  }
+
+  public function getTableRowsWithPagination($request, $pager = true, $returnJson = true)
+  {
+    $rows = [];
+    $limit = $offset = $count = $page = $totalPages = 0;
+    $filter = $sortColumn = $sortOrder = '';
+
+    if (!empty($request['filter']))
+    {
+      $filter = $request['filter'];
+    }
+
+    if (!empty($request['sort']) && $request['sort'][0] == '-')
+    {
+      $sortColumn = substr($request['sort'], 1);
+      $sortOrder = 'desc';
+    }
+    else if (!empty($request['sort']))
+    {
+      $sortColumn = $request['sort'];
+      $sortOrder = 'asc';
+    }
+    else
+    {
+      $sortColumn = 'id';
+      $sortOrder = 'desc';
+    }
+
+    if ($pager)
+    {
+      $count = $this->Prerequisite->searchTableRowsWithPagination(true, $limit, $offset, $filter, $sortColumn, $sortOrder);
+      encode_requested_data($request, $count, $limit, $offset, $totalPages, $page);
+    }
+
+    $this->Prerequisite->searchTableRowsWithPagination(false, $limit, $offset, $filter, $sortColumn, $sortOrder)->each(function ($prerequisite) use (&$rows) {
+
+      $id = strval($prerequisite->id);
+      unset($prerequisite->id);
+
+      array_push($rows, [
+        'type' => $this->responseType,
+        'id' => $id,
+        'attributes' => $prerequisite
+      ]);
+    });
+
+    return [
+      'rows' => $rows,
+      'page' => $page,
+      'totalPages' => $totalPages,
+      'records' => $count,
+    ];
+  }
+
+  public function getPrerequisiteByCurriculumSubjectId($id)
+  {
+    return $this->Prerequisite->byCurriculumSubjectId($id);
+  }
+
+  public function create($request)
+  {
+    $prerequisite = $this->Prerequisite->create($request->all());
+    $id = strval($prerequisite->id);
+    unset($prerequisite->id);
+
+    return [
+      'success' => true,
+      'prerequisite' => $prerequisite,
+      'id' => $id,
+    ];
+  }
+
+  public function update($request, $id)
+  {
+    $prerequisite = $this->Prerequisite->byId($id);
+
+    if (empty($prerequisite)) {
+      return [
+        'success' => false,
+      ];
+    }
+
+    $this->Prerequisite->update($request->all(), $prerequisite);
+    $prerequisite = $this->Prerequisite->byId($id);
+    unset($prerequisite->id);
+
+    return [
+      'success' => true,
+      'prerequisite' => $prerequisite,
+      'id' => $id,
+    ];
+
+  }
+
+  public function delete($id)
+  {
+    $Prerequisite = $this->Prerequisite->byId($id);
+
+    if (empty($Prerequisite)) {
+      return false;
+    }
+
+    $this->Prerequisite->delete($id);
+
+    return true;
+  }
+}
