7ca51c16fbebc7eb09615f63265aa5df5b9772eb
f-molina <00300917@uca.edu.sv>
2022-05-18 16:49:40 -0700

    [ADDED] curricula apidoc

--- app/Http/Controllers/CurriculumController.php
+++ app/Http/Controllers/CurriculumController.php
@@ -37,6 +37,40 @@ class CurriculumController extends Controller
    *
    * @return \Illuminate\Http\Response
    */
+   /** 
+   *  @OA\Get(
+   *    path="/api/curricula",
+   *    operationId="getCurricula",
+   *    tags={"Curricula"},
+   * security={{"bearer_token":{}}},
+   *    summary="Get curricula list",
+   *    description="Returns curricula list",
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      )
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function index()
   {
     $response = $this->CurriculumManagerService->getTableRowsWithPagination(request()->all());
@@ -60,6 +94,80 @@ class CurriculumController extends Controller
    * @param  \Illuminate\Http\Request  $request
    * @return \Illuminate\Http\Response
    */
+   /** 
+   *  @OA\Post(
+   *    path="/api/curricula",
+   *    operationId="postCurricula",
+   *    tags={"Curricula"},
+   * security={{"bearer_token":{}}},
+   *    summary="Create curricula",
+   *    description="Create curricula",
+   * 
+   *    @OA\Parameter(
+   *      name="name",
+   *      in="query",
+   *      description="Curricula name to create",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="string",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="year",
+   *      in="query",
+   *      description="Curricula year",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="is_active",
+   *      in="query",
+   *      description="Is curricula active?",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="boolean",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="is_approved",
+   *      in="query",
+   *      description="Is curricula currently approved?",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="boolean",
+   *      )
+   *    ),
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      ),
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function store(CurriculumRequest $request)
   {
     $response = $this->CurriculumManagerService->create($request);
@@ -82,6 +190,50 @@ class CurriculumController extends Controller
    * @param  \App\Models\Curriculum  $Curriculum
    * @return \Illuminate\Http\Response
    */
+  /** 
+   *  @OA\Get(
+   *    path="/api/curricula/{id}",
+   *    operationId="get curricula by id",
+   *    tags={"Curricula"},
+   * security={{"bearer_token":{}}},
+   *    summary="Get curricula by id",
+   *    description="Returns curricula by id",
+   * 
+   *    @OA\Parameter(
+   *      name="id",
+   *      in="path",
+   *      description="Curricula id",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer"
+   *      )
+   *    ),
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      )
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function show($id)
   {
     $curriculum = $this->CurriculumManagerService->getCurriculum($id);
@@ -121,6 +273,89 @@ class CurriculumController extends Controller
    * @param  \App\Models\Curriculum  $Curriculum
    * @return \Illuminate\Http\Response
    */
+    /** 
+   *  @OA\Put(
+   *    path="/api/curricula/{id}",
+   *    operationId="putCurricula",
+   *    tags={"Curricula"},
+   * security={{"bearer_token":{}}},
+   *    summary="Update curricula",
+   *    description="Update curricula",
+   * 
+   *    @OA\Parameter(
+   *      name="id",
+   *      in="path",
+   *      required=true,
+   *      description="Curricula id",
+   *      @OA\Schema(
+   *        type="integer"
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="name",
+   *      in="query",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="string"
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="year",
+   *      in="query",
+   *      description="Curricula year",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="is_active",
+   *      in="query",
+   *      description="Is curricula active?",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="boolean",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="is_approved",
+   *      in="query",
+   *      description="Is curricula currently approved?",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="boolean",
+   *      )
+   *    ),
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      )
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function update(CurriculumRequest $request, $data)
   {
     $response = $this->CurriculumManagerService->update($request, $data);
@@ -156,6 +391,50 @@ class CurriculumController extends Controller
    * @param  \App\Models\Curriculum  $Curriculum
    * @return \Illuminate\Http\Response
    */
+  /** 
+   *  @OA\Delete(
+   *    path="/api/curricula/{id}",
+   *    operationId="delete curricula by id",
+   *    tags={"Curricula"},
+   * security={{"bearer_token":{}}},
+   *    summary="Delete curricula by id",
+   *    description="Deletes curricula by id",
+   * 
+   *    @OA\Parameter(
+   *      name="id",
+   *      in="path",
+   *      description="Curricula id",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer"
+   *      )
+   *    ),
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      )
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function destroy($request)
   {
     $response = $this->CurriculumManagerService->delete($request);
