8848d16324ce482e7fbedd07db1ecb3290c7cbc8
f-molina <00300917@uca.edu.sv>
2022-05-18 17:41:08 -0700

    [ADDED] sections apidoc

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -37,6 +37,40 @@ class SectionController extends Controller
    *
    * @return \Illuminate\Http\Response
    */
+     /** 
+   *  @OA\Get(
+   *    path="/api/sections",
+   *    operationId="getSections",
+   *    tags={"Sections"},
+   * security={{"bearer_token":{}}},
+   *    summary="Get section list",
+   *    description="Returns section list",
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      )
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function index()
   {
     $response = $this->SectionManagerService->getTableRowsWithPagination(request()->all());
@@ -60,6 +94,90 @@ class SectionController extends Controller
    * @param  \Illuminate\Http\Request  $request
    * @return \Illuminate\Http\Response
    */
+     /** 
+   *  @OA\Post(
+   *    path="/api/sections",
+   *    operationId="postSection",
+   *    tags={"Sections"},
+   * security={{"bearer_token":{}}},
+   *    summary="Create section",
+   *    description="Create section",
+   * 
+   *    @OA\Parameter(
+   *      name="quota",
+   *      in="query",
+   *      description="Section quota",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="schedule",
+   *      in="query",
+   *      description="Section schedule",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="string",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="teacher_id",
+   *      in="query",
+   *      description="Section teacher id",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="curriculum_subject_id",
+   *      in="query",
+   *      description="Section curriculum_subject_id",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *   @OA\Parameter(
+   *      name="period_id",
+   *      in="query",
+   *      description="Section period_id",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      ),
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function store(SectionRequest $request)
   {
     $response = $this->SectionManagerService->create($request);
@@ -82,6 +200,50 @@ class SectionController extends Controller
    * @param  \App\Models\Section  $Section
    * @return \Illuminate\Http\Response
    */
+    /** 
+   *  @OA\Get(
+   *    path="/api/sections/{code}",
+   *    operationId="get section by code",
+   *    tags={"Sections"},
+   * security={{"bearer_token":{}}},
+   *    summary="Get section by id",
+   *    description="Returns section by id",
+   * 
+   *    @OA\Parameter(
+   *      name="code",
+   *      in="path",
+   *      description="Section code",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer"
+   *      )
+   *    ),
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      )
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function show($id)
   {
     $section = $this->SectionManagerService->getSection($id);
@@ -117,6 +279,100 @@ class SectionController extends Controller
    * @param  \App\Models\Section  $Section
    * @return \Illuminate\Http\Response
    */
+      /** 
+   *  @OA\Put(
+   *    path="/api/sections/{code}",
+   *    operationId="putSection",
+   *    tags={"Sections"},
+   * security={{"bearer_token":{}}},
+   *    summary="Update section",
+   *    description="Update section",
+   * 
+   *    @OA\Parameter(
+   *      name="code",
+   *      in="path",
+   *      required=true,
+   *      description="Section code",
+   *      @OA\Schema(
+   *        type="integer"
+   *      )
+   *    ),
+   * 
+   *    *    @OA\Parameter(
+   *      name="quota",
+   *      in="query",
+   *      description="Section quota",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="schedule",
+   *      in="query",
+   *      description="Section schedule",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="string",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="teacher_id",
+   *      in="query",
+   *      description="Section teacher id",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *    @OA\Parameter(
+   *      name="curriculum_subject_id",
+   *      in="query",
+   *      description="Section curriculum_subject_id",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *   @OA\Parameter(
+   *      name="period_id",
+   *      in="query",
+   *      description="Section period_id",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer",
+   *      )
+   *    ),
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      )
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function update(SectionRequest $request, $data)
   {
     $response = $this->SectionManagerService->update($request, $data);
@@ -152,6 +408,50 @@ class SectionController extends Controller
    * @param  \App\Models\Section  $Section
    * @return \Illuminate\Http\Response
    */
+    /** 
+   *  @OA\Delete(
+   *    path="/api/sections/{code}",
+   *    operationId="delete section by code",
+   *    tags={"Sections"},
+   * security={{"bearer_token":{}}},
+   *    summary="Delete section by code",
+   *    description="Deletes section by code",
+   * 
+   *    @OA\Parameter(
+   *      name="code",
+   *      in="path",
+   *      description="Section code",
+   *      required=true,
+   *      @OA\Schema(
+   *        type="integer"
+   *      )
+   *    ),
+   * 
+   *    @OA\Response(
+   *      response=200,
+   *      description="Success",
+   *      @OA\MediaType(
+   *        mediaType="application/json",
+   *      )
+   *    ),
+   *    @OA\Response(
+   *      response=401,
+   *      description="Unauthenticated",
+   *    ),
+   *    @OA\Response(
+   *      response=403,
+   *      description="Forbidden",
+   *    ),
+   *    @OA\Response(
+   *      response=400,
+   *      description="Bad Request"
+   *    ),
+   *    @OA\Response(
+   *      response=404,
+   *      description="Not Found"
+   *    )
+   *  )
+  */
   public function destroy($request)
   {
     $response = $this->SectionManagerService->delete($request);
