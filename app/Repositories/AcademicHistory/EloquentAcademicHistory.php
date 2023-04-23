f68cb56aabaf25b536b315f7944f416b1aa69573
renemel <rene.melendeza@gmail.com>
2023-04-23 17:08:53 -0600

    [ADDED] SAcademicHistoryQueryFixes

--- app/Repositories/AcademicHistory/EloquentAcademicHistory.php
+++ app/Repositories/AcademicHistory/EloquentAcademicHistory.php
@@ -113,13 +113,17 @@ class EloquentAcademicHistory implements AcademicHistoryInterface
         'e.final_score',
         'e.is_approved',
         DB::raw("concat(t.name,' ',t.last_name) as teachername"),
-        'e.enrollment'
+        'e.enrollment',
+        'p.year',
+        'p.code'
       )
       ->join('sections as s','s.id', '=','e.code')
       ->join('teachers as t','t.id', '=','s.teacher_id')
-      ->join('subjects as s2','s2.id', '=','e.curriculum_subject_id');
-      
-    
+      ->join('subjects as s2','s2.id', '=','e.curriculum_subject_id')
+      ->join('periods as p','p.id','=','s.period_id')
+      ->where('e.student_id','=',$id);
+
+
 
     return new Collection(
       $query->get()
