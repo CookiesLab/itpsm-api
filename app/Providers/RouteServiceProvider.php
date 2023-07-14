1808f67c0702605ba9c04fef2593e32369f6ed2f
renemel <rene.melendeza@gmail.com>
2023-07-14 14:50:13 -0600

    [add] Env route for api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            $apiPrefix = env`('app.API_PREFIX', 'api');
+            $apiPrefix = env('app.API_PREFIX', 'api');
             Route::prefix($apiPrefix)
                 ->middleware('api')
                 ->group(base_path('routes/api.php'));
